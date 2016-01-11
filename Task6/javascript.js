5e695ac146d042c59ef6686a39d9966a46a057d0
danielpetrov <danielpetrov222@gmail.com>
2016-01-11 13:11:30 +0200

    task9 and task6 modif

--- Task6/javascript.js
+++ Task6/javascript.js
@@ -2,9 +2,9 @@ FirstPart = '<img src="image';
 LastPart = '.jpg" height="500" width="500">';
 NumberOfImagesToRotate = 4;
 
-function printImage() {
+function printRandomImage() {
     var rand = Math.ceil(Math.random() * NumberOfImagesToRotate);
-    document.write(FirstPart + rand + LastPart);
+    document.write(FirstPart + rand + LastPart); // document.write - one of the problematics of javascript
 }
 
-printImage();
+printRandomImage();
---
+++ Task9/javascript.js
@@ -0,0 +1,3 @@
+/**
+ * Created by daniel on 11.1.2016 г..
+ */
